e502d9aec9e46c85b7df19b972c3dd9761e9f4ef
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-09-13 20:21:44 -0300

    adiciona input de altura e largura da primeira parede

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,34 @@
 import React, { useState } from 'react';
 
+import Input from './Input';
+
 export default function Form() {
+  const [firstHeightValue, setFirstHeightValue] = useState(2.20);
+
+  const [firstWidthValue, setFirstWidthValue] = useState(1);
+
   return (
     <form>
-      <h1>Hello Code Challenge!</h1>
+      <h1 className="h2">Cálculo da área de pintura:</h1>
+      <div>
+        <section>
+          <h2>Primeira parede:</h2>
+          <div>
+            <Input
+              label="Altura: "
+              value={ firstHeightValue }
+              setValue={ setFirstHeightValue }
+            />
+          </div>
+          <div>
+            <Input
+              label="Largura: "
+              value={ firstWidthValue }
+              setValue={ setFirstWidthValue }
+            />
+          </div>
+        </section>
+      </div>
     </form>
   );
 };
