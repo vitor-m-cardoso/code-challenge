e76dc250b3f86391854d51a2423509ae7be5c484
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-09-14 00:34:44 -0300

    adiciona action e reducer para validar area das portas e janelas e adiciona funcoes responsaveis por calcular se a area das portas e janelas excedem 50% da area da parede

--- src/components/CalcAndResult.js
+++ src/components/CalcAndResult.js
@@ -1,6 +1,116 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import {
+  firstDoorIsValid,
+  secondDoorIsValid,
+  thirdDoorIsValid,
+  fourthDoorIsValid,
+  firstWindowIsValid,
+  secondWindowIsValid,
+  thirdWindowIsValid,
+  fourthWindowIsValid,
+} from '../redux/actions/isValidAreaActions';
 
 export default function CalcAndResult() {
+  const firstWallArea = useSelector((state) => state.wallsReducer.firstWall);
+  const secondWallArea = useSelector((state) => state.wallsReducer.secondWall);
+  const thirdWallArea = useSelector((state) => state.wallsReducer.thirdWall);
+  const fourthWallArea = useSelector((state) => state.wallsReducer.fourthWall);
+  
+  const firstDoorArea = useSelector((state) => state.doorsAndWindowsReducer.firstWall.areaDoorNum);
+  const secondDoorArea = useSelector((state) => state.doorsAndWindowsReducer.secondWall.areaDoorNum);
+  const thirdDoorArea = useSelector((state) => state.doorsAndWindowsReducer.thirdWall.areaDoorNum);
+  const fourthDoorArea = useSelector((state) => state.doorsAndWindowsReducer.fourthWall.areaDoorNum);
+  
+  const firstWindowArea = useSelector((state) => state.doorsAndWindowsReducer.firstWall.areaWindowNum);
+  const secondWindowArea = useSelector((state) => state.doorsAndWindowsReducer.secondWall.areaWindowNum);
+  const thirdWindowArea = useSelector((state) => state.doorsAndWindowsReducer.thirdWall.areaWindowNum);
+  const fourthWindowArea = useSelector((state) => state.doorsAndWindowsReducer.fourthWall.areaWindowNum);
+  
+  const maxDoorAndWindowFirstArea = (parseFloat(firstWallArea) / 2).toFixed(2);
+  const maxDoorAndWindowSecondArea = (parseFloat(secondWallArea) / 2).toFixed(2);
+  const maxDoorAndWindowThirdArea = (parseFloat(thirdWallArea) / 2).toFixed(2);
+  const maxDoorAndWindowFourthArea = (parseFloat(fourthWallArea) / 2).toFixed(2);
+
+  const dispatch = useDispatch();
+
+  const calcFirstArea = () => {
+    let result;
+
+    if (parseFloat(firstDoorArea) > parseFloat(maxDoorAndWindowFirstArea)) {
+      dispatch(firstDoorIsValid(false));
+      result = 0;
+    };
+    if (parseFloat(firstWindowArea) > parseFloat(maxDoorAndWindowFirstArea)) {
+      dispatch(firstWindowIsValid(false));
+      result = 0;
+    };
+    if (parseFloat(firstDoorArea) < parseFloat(maxDoorAndWindowFirstArea) && parseFloat(firstWindowArea) < parseFloat(maxDoorAndWindowFirstArea)) {
+      dispatch(firstDoorIsValid(true));
+      dispatch(firstWindowIsValid(true));
+      result = (parseFloat(firstWallArea) - (parseFloat(firstDoorArea) + parseFloat(firstWindowArea))).toFixed(2);
+    };
+    return result;
+  };
+
+  const calcSecondArea = () => {
+    let result;
+
+    if (parseFloat(secondDoorArea) > parseFloat(maxDoorAndWindowSecondArea)) {
+      dispatch(secondDoorIsValid(false));
+      result = 0;
+    };
+    if (parseFloat(secondWindowArea) > parseFloat(maxDoorAndWindowSecondArea)) {
+      dispatch(secondWindowIsValid(false));
+      result = 0;
+    };
+    if (parseFloat(secondDoorArea) < parseFloat(maxDoorAndWindowSecondArea) && parseFloat(secondWindowArea) < parseFloat(maxDoorAndWindowSecondArea)) {
+      dispatch(secondDoorIsValid(true));
+      dispatch(secondWindowIsValid(true));
+      result = (parseFloat(secondWallArea) - (parseFloat(secondDoorArea) + parseFloat(secondWindowArea))).toFixed(2);
+    };
+    return result;
+  };
+
+  const calcThirdArea = () => {
+    let result;
+
+    if (parseFloat(thirdDoorArea) > parseFloat(maxDoorAndWindowThirdArea)) {
+      dispatch(thirdDoorIsValid(false));
+      result = 0;
+    };
+    if (parseFloat(thirdWindowArea) > parseFloat(maxDoorAndWindowThirdArea)) {
+      dispatch(thirdWindowIsValid(false));
+      result = 0;
+    };
+    if (parseFloat(thirdDoorArea) < parseFloat(maxDoorAndWindowThirdArea) && parseFloat(thirdWindowArea) < parseFloat(maxDoorAndWindowThirdArea)) {
+      dispatch(thirdDoorIsValid(true));
+      dispatch(thirdWindowIsValid(true));
+      result = (parseFloat(thirdWallArea) - (parseFloat(thirdDoorArea) + parseFloat(thirdWindowArea))).toFixed(2);
+    };
+    return result;
+  };
+
+  const calcFourthArea = () => {
+    let result;
+
+    if (parseFloat(fourthDoorArea) > parseFloat(maxDoorAndWindowFourthArea)) {
+      dispatch(fourthDoorIsValid(false));
+      result = 0;
+    };
+    if (parseFloat(fourthWindowArea) > parseFloat(maxDoorAndWindowFourthArea)) {
+      dispatch(fourthWindowIsValid(false));
+      result = 0;
+    };
+    if (parseFloat(fourthDoorArea) < parseFloat(maxDoorAndWindowFourthArea) && parseFloat(fourthWindowArea) < parseFloat(maxDoorAndWindowFourthArea)) {
+      dispatch(fourthDoorIsValid(true));
+      dispatch(fourthWindowIsValid(true));
+      result = (parseFloat(fourthWallArea) - (parseFloat(fourthDoorArea) + parseFloat(fourthWindowArea))).toFixed(2);
+    };
+    return result;
+  };
+
   return (
     <section>
       <h1 className="h2">Resultado:</h1>
