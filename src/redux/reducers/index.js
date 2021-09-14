5815b9f28d642b36a6009486b4c46981024acbb7
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-09-14 00:59:47 -0300

    CalcAndResult: quebra linha ao final, reducer index: adiciona isValidAreaReducer ao combineReducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import wallsReducer from './wallsReducer';
 import doorsAndWindowsReducer from './doorsAndWindowsReducer';
 import selectedPaintReducer from './selectedPaintReducer';
+import isValidAreaReducer from './isValidAreaReducer';
 
 const rootReducer = combineReducers({
   wallsReducer,
   doorsAndWindowsReducer,
   selectedPaintReducer,
+  isValidAreaReducer,
 });
 
 export default rootReducer;
